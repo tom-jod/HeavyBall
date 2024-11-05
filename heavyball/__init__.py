91b4ee5a95b32cbb73954f033210197438361567
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2024-11-05 13:15:53 +0100

    add SFAdamW

--- heavyball/__init__.py
+++ heavyball/__init__.py
@@ -1,3 +1,4 @@
 from .palm_foreach_soap import PalmForEachSoap
+from .palm_foreach_sfadamw import PaLMForeachSFAdamW
 
-__all__ = ['PalmForEachSoap']
+__all__ = ['PalmForEachSoap', 'PaLMForeachSFAdamW']
