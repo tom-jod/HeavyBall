cd18df9b164f3dbb1c477d1bd340d760c23bba1b
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2024-11-10 21:33:05 +0100

    better test

--- test/test_opt.py
+++ test/test_opt.py
@@ -4,69 +4,66 @@ import inspect
 
 import pytest
 import torch
+import torch.backends.opt_einsum
 import torch.nn as nn
 from torch.backends import cudnn
 
 import heavyball.utils
-from heavyball import PaLMForeachSOAP, SFPaLMForeachSOAP, PaLMForeachSFAdamW, PrecondScheduleSFPaLMSOAP, ForeachADOPT, \
-    ForeachSOAP, ForeachSFAdamW, ForeachLaProp, PrecondScheduleForeachSOAP, PrecondSchedulePaLMForeachSOAP
+from heavyball import ForeachSOAP, ForeachPSGDKron, ForeachSFAdamW, ForeachADOPT
 
-steps = 1000
+steps = 10_000
 cudnn.benchmark = True
 cudnn.deterministic = False
 torch.use_deterministic_algorithms(False)
 torch.set_float32_matmul_precision("high")  # highest: FP32, high: TF32, medium: bf16
+torch.backends.opt_einsum.enabled = True
+torch.backends.opt_einsum.strategy = "optimal"
 
-args = {'betas': (0.9, 0.95), 'precondition_frequency': 1, 'merge_dims': False, 'warmup_steps': 100,
-        'max_precond_dim': 2 ** 16, 'beta': 0.9}
+args = {'betas': (0.9, 0.95), 'precondition_frequency': 4, 'merge_dims': False, 'warmup_steps': 100,
+        'max_precond_dim': 2 ** 16, 'beta': 0.9, 'preconditioner_update_probability': 1 / 4}
 
 
 def data(size, batch, dtype):
-    inp = torch.randn((batch, size), device='cuda', dtype=dtype)
-    tgt = (inp.cumsum(1) - torch.flip(inp, [1]).cumsum(1)) / size ** 0.5
-    return inp, tgt
+    inp = torch.randn((batch, size, 1), device='cuda', dtype=torch.float)
+    i0 = inp > 0
+    return i0.to(dtype), (i0.sum(1) % 2).to(dtype)
 
 
-@pytest.mark.parametrize('method',
-                         ['newtonschulz2', 'newtonschulz5', 'newtonschulz10', 'newtonschulz20', 'newtonschulz50'])
-@pytest.mark.parametrize('opt', [PaLMForeachSOAP, SFPaLMForeachSOAP, PaLMForeachSFAdamW, PrecondScheduleSFPaLMSOAP,
-                                 ForeachADOPT, ForeachSOAP, ForeachSFAdamW, ForeachLaProp, PrecondScheduleForeachSOAP,
-                                 PrecondSchedulePaLMForeachSOAP, torch.optim.AdamW, torch.optim.Adam])
-@pytest.mark.parametrize('dtype', [torch.float32, torch.bfloat16, torch.double])
-@pytest.mark.parametrize('size,batch', [(128, 32)])
-@pytest.mark.parametrize('lr', [3e-2, 1e-2, 3e-3, 1e-3, 3e-4, 1e-4])
+@pytest.mark.parametrize('method', ['qr'])
+@pytest.mark.parametrize('dtype', [torch.float32])
+@pytest.mark.parametrize('length,size,batch', [(16, 32, 32)])  # 21, 34 for (16, 32) without compile
+@pytest.mark.parametrize('opt', [ForeachSFAdamW, ForeachADOPT, ForeachSOAP, ForeachPSGDKron])
 @pytest.mark.parametrize('weight_decay', [0])
-def test_f(opt, dtype, size, batch, lr, weight_decay, method):
+def test_f(opt, dtype, size, batch, weight_decay, method, length):
+    if "soap" not in opt.__name__.lower() and method != 'qr':
+        return
     heavyball.utils.zeroth_power_mode = method
-    torch.cuda.empty_cache()
-    gc.collect()
-    torch.manual_seed(0x1239121)
 
-    a = nn.Sequential(nn.Linear(size, size, bias=False), nn.ReLU(), nn.LayerNorm(size),
-                      nn.Linear(size, size)).cuda().to(dtype)
-    signature = inspect.signature(opt)
-    o = opt(a.parameters(), lr, weight_decay=weight_decay,
-            **{k: v for k, v in args.items() if k in signature.parameters})
-    torch.cuda.empty_cache()
-    gc.collect()
+    for lr in [1e-1, 3e-2, 1e-2, 3e-3, 1e-3, 3e-4, 1e-4]:
+        torch.cuda.empty_cache()
+        gc.collect()
+        torch.manual_seed(0x1239121)
 
-    loss_mean = 0
-    start = datetime.datetime.now()
+        a = nn.LSTM(1, size, proj_size=1, batch_first=True).cuda().train()
+        signature = inspect.signature(opt)
+        o = opt(a.parameters(), lr, weight_decay=weight_decay,
+                **{k: v for k, v in args.items() if k in signature.parameters})
+        torch.cuda.empty_cache()
+        gc.collect()
 
-    for i in range(steps):
-        inp, tgt = data(size, batch, dtype)
-        loss = (a(inp) - tgt).square().mean()
-        loss.backward()
-        o.step()
-        o.zero_grad()
-        with torch.no_grad():
-            loss_mean += loss.detach() / steps
+        loss_mean = 0
+        start = datetime.datetime.now()
 
-    if hasattr(o, 'eval'):
-        o.eval()
-
-    inp, tgt = data(size, batch, dtype)
-    eval_loss = (a(inp) - tgt).square().mean()
-    dist = datetime.datetime.now() - start
-    print(f'Took {dist} | {opt.__name__}, {dtype=}, {size=}, {batch=}, {lr=}, {weight_decay=}, {method=} | '
-          f'Loss: {loss.item()} - Eval: {eval_loss.item()} - Mean: {loss_mean.item()}')
+        for i in range(steps):
+            inp, tgt = data(length, batch, dtype)
+            loss = torch.nn.functional.binary_cross_entropy_with_logits(a(inp)[0][:, -1], tgt)
+            loss.backward()
+            o.step()
+            o.zero_grad()
+            with torch.no_grad():
+                loss_mean += loss.detach() / steps
+            if loss.item() < 0.1:
+                dist = datetime.datetime.now() - start
+                print(f'Took {dist} | {opt.__name__}, {dtype=}, {size=}, {length=}, {batch=}, {lr=}, {weight_decay=}, {method=} | Iteration: {i}')
+                return
+    raise ValueError(f"Failed to converge")
