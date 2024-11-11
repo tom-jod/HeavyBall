260ecf4726bd2fd1a7a08331d6ae48d3d58f5f75
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2024-11-11 12:35:16 +0100

    simplify test task

--- test/test_opt.py
+++ test/test_opt.py
@@ -12,7 +12,7 @@ from torch.backends import cudnn
 import heavyball.utils
 from heavyball import ForeachSOAP, ForeachPSGDKron, ForeachSFAdamW, ForeachADOPT
 
-steps = 10_000
+steps = 1_000
 cudnn.benchmark = True
 cudnn.deterministic = False
 torch.use_deterministic_algorithms(False)
@@ -20,7 +20,7 @@ torch.set_float32_matmul_precision("high")  # highest: FP32, high: TF32, medium:
 torch.backends.opt_einsum.enabled = True
 torch.backends.opt_einsum.strategy = "optimal"
 
-args = {'betas': (0.9, 0.95), 'precondition_frequency': 4, 'merge_dims': False, 'warmup_steps': 100,
+args = {'betas': (0.9, 0.999), 'precondition_frequency': 4, 'merge_dims': False, 'warmup_steps': 100,
         'max_precond_dim': 2 ** 16, 'beta': 0.9, 'preconditioner_update_probability': 1 / 4}
 
 
@@ -28,16 +28,30 @@ def data(size, batch, dtype):
     inp = torch.randn((batch, size, 1), device='cuda', dtype=torch.float)
     inp = inp > 0
     i0, i1 = inp.chunk(2, 1)
-    i0 = torch.logical_xor(i0, i1)
-    return inp.to(dtype), i0.to(dtype)
+    xored = torch.logical_xor(i0, i1)
+    return inp.to(dtype), xored.to(dtype)
+
+
+class Model(nn.Module):
+    def __init__(self, size, depth):
+        super().__init__()
+        self.enc = nn.LSTM(1, size, depth, batch_first=True)
+        self.dec = nn.LSTM(1, size, depth, batch_first=True)
+        self.proj = nn.Linear(size, 1, bias=False)
+
+    def forward(self, inp):
+        i0, i1 = inp.chunk(2, 1)
+        _, state = self.enc(i0)
+        out, _ = self.dec(i1, state)
+        return self.proj(out)
 
 
 @pytest.mark.parametrize('method', ['qr'])
 @pytest.mark.parametrize('dtype', [torch.float32])
-@pytest.mark.parametrize('length,size,batch', [(16, 32, 32)])  # 21, 34 for (16, 32) without compile
+@pytest.mark.parametrize('length,size,depth,batch', [(16, 128, 1, 32)])  # 21, 34 for (16, 32) without compile
 @pytest.mark.parametrize('opt', [ForeachSFAdamW, ForeachADOPT, ForeachSOAP, ForeachPSGDKron])
 @pytest.mark.parametrize('weight_decay', [0])
-def test_f(opt, dtype, size, batch, weight_decay, method, length):
+def test_f(opt, dtype, size, batch, weight_decay, method, length, depth):
     if "soap" not in opt.__name__.lower() and method != 'qr':
         return
     heavyball.utils.zeroth_power_mode = method
@@ -50,9 +64,9 @@ def test_f(opt, dtype, size, batch, weight_decay, method, length):
         gc.collect()
         torch.manual_seed(0x1239121)
 
-        a = nn.LSTM(1, size, proj_size=1, batch_first=True).cuda().train()
+        m = torch.compile(Model(size, depth).to(dtype).cuda(), mode='max-autotune', fullgraph=False, dynamic=False)
         signature = inspect.signature(opt)
-        o = opt(a.parameters(), lr, weight_decay=weight_decay,
+        o = opt(m.parameters(), lr, weight_decay=weight_decay,
                 **{k: v for k, v in args.items() if k in signature.parameters})
         torch.cuda.empty_cache()
         gc.collect()
@@ -61,14 +75,15 @@ def test_f(opt, dtype, size, batch, weight_decay, method, length):
 
         for i in range(steps):
             inp, tgt = data(length, batch, dtype)
-            loss = torch.nn.functional.binary_cross_entropy_with_logits(a(inp)[0].chunk(2, 1)[1], tgt)
+            out = m(inp)
+            loss = torch.nn.functional.binary_cross_entropy_with_logits(out, tgt)
             loss.backward()
             o.step()
             o.zero_grad()
             if loss.item() < 0.1:
                 dist = datetime.datetime.now() - start
-                print(
-                    f'Took {dist} | {opt.__name__}, {dtype=}, {size=}, {length=}, {batch=}, {lr=}, {weight_decay=}, {method=} | Iteration: {i}')
+                print(f'Took {dist} | {opt.__name__}, {dtype=}, {size=}, {length=}, {batch=}, {lr=}, {weight_decay=}, '
+                      f'{method=} | Iteration: {i}')
                 return
 
         lrs.append(lr)
