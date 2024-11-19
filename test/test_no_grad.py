cfe395b6769804f19c9d2ae3bb44c2fae2f352cf
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2024-11-19 14:50:56 +0100

    add closure test, no_grad test

---
+++ test/test_no_grad.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import heavyball
+import heavyball.utils
+import pytest
+import torch
+from benchmark.utils import get_optim
+from heavyball.utils import set_torch, clean
+from torch import nn
+
+
+class Param(nn.Module):
+    def __init__(self, size):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(size))
+
+    def forward(self, inp):
+        return self.weight.mean() * inp
+
+
+@pytest.mark.parametrize("opt", heavyball.__all__)
+@pytest.mark.parametrize("size", [(4, 4, 4, 4), ])
+def test_closre(opt, size: List[int], depth: int = 2, iterations: int = 5, outer_iterations: int = 3):
+    clean()
+    set_torch()
+
+    opt = getattr(heavyball, opt)
+
+    for _ in range(outer_iterations):
+        clean()
+        model = nn.Sequential(*[Param(size) for _ in range(depth)]).cuda()
+        o = get_optim(opt, model.parameters(), lr=1e-3)
+
+        for i in range(iterations):
+            o.step()
+            o.zero_grad()
+            assert o.state_size() == 0
+
+        del model, o
