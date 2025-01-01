297f1d95fb33cc00268a6d086ea498c5d8df9360
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2025-01-01 14:26:29 +0100

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     name='heavyball',
     license='BSD',
     description='Efficient optimizers',
-    version='1.3.1',
+    version='1.4.0',
     long_description=README,
     url='https://github.com/clashluke/heavyball',
     packages=setuptools.find_packages(),
