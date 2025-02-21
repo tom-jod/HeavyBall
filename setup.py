d205a2734b6329f886b8ee008e6773a6826b6b5f
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2025-02-21 14:28:44 +0100

    bump version for SOAP fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     name='heavyball',
     license='BSD',
     description='Efficient optimizers',
-    version='1.5.3',
+    version='1.6.0',
     long_description=README,
     url='https://github.com/clashluke/heavyball',
     packages=setuptools.find_packages(),
