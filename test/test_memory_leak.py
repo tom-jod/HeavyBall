98860d905468ef739b33743f37d1167371bbbdac
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2025-04-01 15:42:28 +0200

    leak test works

--- test/test_memory_leak.py
+++ test/test_memory_leak.py
@@ -1,6 +1,8 @@
 import pytest
 import torch
+import tqdm
 from torch import nn
+from torch.nn import functional as F
 
 import heavyball
 import heavyball.utils
@@ -20,38 +22,63 @@ def get_memory():
     return out
 
 
-@pytest.mark.parametrize("opt", ["NewtonHybrid2PSGDKron", "ForeachPSGDKron", "ForeachADOPT", "SOAP"])
-@pytest.mark.parametrize("size,depth", [(256, 1), (128, 4)])
-@pytest.mark.parametrize("mars", [True, False])
-@pytest.mark.parametrize("merge_dims", [True, False])
-@pytest.mark.parametrize("split", [True, False])
-@pytest.mark.parametrize("finite_differences", [True, False])
+class LayerNorm2dParam(nn.Module):
+    def __init__(self, num_features):
+        super(LayerNorm2dParam, self).__init__()
+        self.param = nn.Parameter(torch.ones(2, num_features))
+
+    def forward(self, x):
+        weight, bias = self.param.unbind(0)
+        return F.layer_norm(x, [x.size(-1)], weight, bias)
+
+
+@pytest.mark.parametrize("opt", ["NewtonHybrid2PSGDKron"])
+@pytest.mark.parametrize("size,depth", [(64, 2)])
+@pytest.mark.parametrize("mars", [True])
+@pytest.mark.parametrize("cached", [True])
+@pytest.mark.parametrize("delayed", [True])
+@pytest.mark.parametrize("merge_dims", [True])
+@pytest.mark.parametrize("split", [False])
+@pytest.mark.parametrize("finite_differences", [False])
 def test_memory(
     opt,
     size,
     depth: int,
     mars: bool,
+    cached: bool,
+    delayed: bool,
     merge_dims: bool,
     split: bool,
     finite_differences: bool,
-    iterations: int = 500,
+    iterations: int = 10000,
     warmup: int = 100,
-    max_growth: float = 1.05,
+    check_every: int = 10,
+    max_growth: float = 1.10,
 ):
     set_torch()
 
     opt = getattr(heavyball, opt)
-    model = nn.Sequential(*[nn.Linear(size, size) for _ in range(depth)]).cuda()
+    model = nn.Sequential(*[LayerNorm2dParam(size) for _ in range(depth)]).cuda()
     print(model)
 
-    o = get_optim(opt, model.parameters(), lr=1e-3, mars=mars, merge_dims=merge_dims, split=split)
+    o = get_optim(
+        opt,
+        model.parameters(),
+        lr=1e-3,
+        mars=mars,
+        merge_dims=merge_dims,
+        split=split,
+        cached=cached,
+        delayed=delayed,
+        preconditioner_update_probability=1.0,
+    )
     if finite_differences:
         if not o.hessian_approx:
             pytest.skip("Finite Differences is an HVP calculation - can't do it on non-hvp optimizers")
         o.finite_differences = True
 
     peak = 0
-    for i in range(iterations):
+    for i in tqdm.trange(iterations):
         data = torch.randn((1, size), device="cuda").requires_grad_(True)
 
         def _closure():
@@ -62,7 +89,10 @@ def test_memory(
 
         o.step(_closure)
 
-        if i <= warmup:
-            peak = max(peak, get_memory())
-        if i > warmup:
-            assert peak * max_growth >= get_memory()  # fudge factor
+        if i % check_every == 0:
+            if i <= warmup:
+                peak = max(peak, get_memory())
+            if i > warmup:
+                new = get_memory()
+                print(i, peak, new)
+                assert peak * max_growth >= new  # fudge factor
